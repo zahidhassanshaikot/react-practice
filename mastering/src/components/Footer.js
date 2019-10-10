de0863f573702e50c9db29ae2afc0bc5fd1a8df0
zahidhassanshaikot <zahidhassanshaikot@gmail.com>
2019-10-10 13:46:59 +0545

    html template mastering

---
+++ mastering/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="main-footer">
+                    <div className="pull-right hidden-xs">
+                        <b>Version</b> 2.4.18
+                    </div>
+                    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+                    reserved.
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
